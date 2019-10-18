829a6b84f67cd8ee8e1019ff380fe3ee90f112e8
baian1 <chizhongnan1@gmail.com>
2019-10-18 21:00:42 +0800

    ts 重载

--- javescript/Typescript/实验/重载.ts
+++ javescript/Typescript/实验/重载.ts
@@ -1,5 +1,10 @@
+//重载函数声明
 function reverse(x: number): number;
 function reverse(x: string): string;
+
+//重载函数的实现
+//其类型需要满足前面的声明的函数的类型
+//方便函数内对元素进行区分 实现重载操作
 function reverse(x: number | string): number | string {
   if (typeof x === "number") {
     return Number(
@@ -16,3 +21,5 @@ function reverse(x: number | string): number | string {
       .join("");
   }
 }
+let cc = 1n;
+const a = reverse(cc);
